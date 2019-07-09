2742a92ad2e44f0982f71964010ecaa099249361
mat4297 <matteo.griot@gmail.com>
2019-07-09 19:41:40 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python3 web python3 PythonBot.py
---
+++ PythonBot.py
@@ -0,0 +1,3 @@
+#pip install aiogram https: // github.com/aiogram/aiogram
+#pip install pytelegrambotapi --upgrade https://github.com/eternnoir/pyTelegramBotAPI
+#pip install python-telegram-bot --upgrade https://github.com/python-telegram-bot/python-telegram-bot
